480a323ca8392bde3693c590cc4dda4f08a0416d
Thomas LEFEBVRE <contact@thomas-lefebvre.fr>
2021-09-24 13:03:11 -0700

    add controller test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,12 +2,19 @@
 
 namespace Tests\AppBundle\Controller;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Method;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndexAction()
     {
-        
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode()
+        );
     }
 }

89fc5bac0ade94989a20950e537eef50d7169aa9
Thomas LEFEBVRE <contact@thomas-lefebvre.fr>
2021-09-24 07:06:06 -0700

    travis setup, first dummy test, phpunit test coverage html

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexAction()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        
     }
 }

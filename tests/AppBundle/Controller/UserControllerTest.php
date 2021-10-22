4ee2e234107af5c61c5e7ba2707423a218441657
Thomas LEFEBVRE <contact@thomas-lefebvre.fr>
2021-10-22 18:32:35 +0200

    remove create user for users, add roles in tests

--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -11,18 +11,23 @@ class UserControllerTest extends WebTestCase
     private $username;
     private $email;
     private $password;
+    private $roles;
 
     public function setUp()
     {
         $this->client = static::createClient();
         $this->username = 'TestUser'.time();
         $this->email = 'test-'.uniqid().'@example.com';
-        $this->password = 'test12345';
+        $this->password = 'test1234';
+        $this->roles = 'ROLE_USER';
     }
 
-    public function testListAction()
+    public function testAdminListAction()
     {
-        $this->logIn();
+        // LOAD FIXTURES
+
+        // LIST_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(true);
 
         $crawler = $this->client->request('GET', '/users');
         $this->assertEquals(
@@ -39,11 +44,32 @@ class UserControllerTest extends WebTestCase
             0,
             $crawler->filter('html:contains("Actions")')->count()
         );
+
+        // RELOAD FIXTURES
     }
 
-    public function testCreateAction()
+    public function testUserListAction()
     {
-        $this->logIn();
+        // LOAD FIXTURES
+
+        // LIST_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(false);
+
+        $this->client->request('GET', '/users');
+        $this->assertEquals(
+            Response::HTTP_FORBIDDEN,
+            $this->client->getResponse()->getStatusCode()
+        );
+
+        // RELOAD FIXTURES
+    }
+
+    public function testAdminCreateAction()
+    {
+        // LOAD FIXTURES
+
+        // CREATE_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(true);
 
         $crawler = $this->client->request('GET', '/users/create');
         $this->assertEquals(
@@ -53,46 +79,105 @@ class UserControllerTest extends WebTestCase
 
         $form = $crawler->selectButton('Ajouter')->form();
         $form['user[username]'] = $this->username;
-        $form['user[password][first]'] = $this->password;
+        $form['user[password][first]'] = $this->pasword;
         $form['user[password][second]'] = $this->password;
         $form['user[email]'] = $this->email;
+        $form['user[roles]'] = $this->roles;
         $this->client->submit($form);
 
         $crawler = $this->client->followRedirect();
 
+        // Test that there is a page
         $this->assertGreaterThan(
             0,
             $crawler->filter('table')->count()
         );
 
+        // Test that the username is present in the list
         $this->assertGreaterThan(
             0,
             $crawler->filter('html:contains('.$this->username.')')->count()
         );
+
+        // RELOAD FIXTURES
     }
 
-    public function testEditAction()
+    public function testUserCreateAction()
     {
-        $crawler = $this->client->request('GET', '/users/2/edit');
+        // LOAD FIXTURES
+
+        // CREATE_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(false);
+
+        $this->client->request('GET', '/users/create');
+        $this->assertEquals(
+            Response::HTTP_FORBIDDEN,
+            $this->client->getResponse()->getStatusCode()
+        );
+
+        // RELOAD FIXTURES
+    }
+
+    public function testAdminEditAction()
+    {
+        // LOAD FIXTURES
+
+        // CREATE_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(true);
+
+        $crawler = $this->client->request('GET', '/users/3/edit');
         $this->assertEquals(
             Response::HTTP_OK,
             $this->client->getResponse()->getStatusCode()
         );
 
         $form = $crawler->selectButton('Modifier')->form();
-        $form['user[username]'] = $this->username;
+        $form['user[username]'] = 'nameHasBeenChanged';
         $form['user[password][first]'] = $this->password;
         $form['user[password][second]'] = $this->password;
-        $form['user[email]'] = $this->email;
+        $form['user[email]'] = 'emailHasBeenChanged@example.com';
+        $form['user[roles]'] = $this->roles;
         $this->client->submit($form);
 
+        $crawler = $this->client->followRedirect();
+
+        // Test that the new username is present in the list
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("nameHasBeenChanged")')->count()
+        );
+
+        // Test that the new email is present in the list
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("emailHasBeenChanged@example.com")')->count()
+        );
+
+        // RELOAD FIXTURES
     }
 
-    private function logIn()
+    public function testUserEditAction()
     {
+        // LOAD FIXTURES
+
+        // CREATE_ACTION IS ONLY ACCESSIBLE BY ADMIN
+        $this->logIn(false);
+
+        $crawler = $this->client->request('GET', '/users/3/edit');
+        $this->assertEquals(
+            Response::HTTP_FORBIDDEN,
+            $this->client->getResponse()->getStatusCode()
+        );
+
+        // RELOAD FIXTURES
+    }
+
+    private function logIn(bool $admin)
+    {
+        // USER 1 IS ADMIN, REST IS USER, PASSWORD IS ALWAYS THE SAME
         $crawler = $this->client->request('GET', '/login');
         $form = $crawler->selectButton('Se connecter')->form();
-        $form['_username'] = 'user_1';
+        $form['_username'] = $admin = true ? 'user_1' : 'user_2';
         $form['_password'] = 'test1234';
         $this->client->submit($form);
     }
